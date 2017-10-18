8d6260b1decd7572412e86edee60cfc45b6d4b0a
Danny Guo <dannyguo91@gmail.com>
2017-10-18 11:07:00 -0400

    Configure Netlify deployment

---
+++ webpack/prod.config.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+const path = require('path');
+
+const merge = require('webpack-merge');
+
+const baseConfig = require('./base.config.js');
+
+module.exports = merge(baseConfig, {
+    entry: path.join(__dirname, '../src/entry.js')
+});
